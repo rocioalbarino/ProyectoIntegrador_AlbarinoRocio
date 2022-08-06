11c0e241389a2b2031e93be8664f0eb6886b5e5c
rocioalbarino <100244315+rocioalbarino@users.noreply.github.com>
2022-08-06 00:22:46 -0300

    Routing y creacion de login

---
+++ ProyectoIntegrador_AlbarinoRocio/BackEnd/src/main/java/com/portfolio/rocioalba/Repository/IPersonaRepository.java
@@ -0,0 +1,10 @@
+package com.portfolio.rocioalba.Repository;
+
+import com.portfolio.rocioalba.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona,Long>{
+    
+}
