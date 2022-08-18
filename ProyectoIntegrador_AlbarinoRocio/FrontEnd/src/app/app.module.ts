541af06b790c930b36bee6912872e03f060d3b0d
rocioalbarino <100244315+rocioalbarino@users.noreply.github.com>
2022-08-17 23:48:14 -0300

    Componente experiencia

--- ProyectoIntegrador_AlbarinoRocio/FrontEnd/src/app/app.module.ts
+++ ProyectoIntegrador_AlbarinoRocio/FrontEnd/src/app/app.module.ts
@@ -18,6 +18,8 @@ import {HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { interceptorProvider } from './service/interceptor-service';
+import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
+import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
 
 
 @NgModule({
@@ -34,7 +36,9 @@ import { interceptorProvider } from './service/interceptor-service';
     ProyectoComponent,
     FooterComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    NewExperienciaComponent,
+    EditExperienciaComponent
   ],
   imports: [
     BrowserModule,
