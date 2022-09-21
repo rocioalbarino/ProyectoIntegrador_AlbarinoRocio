ae930bdc92e8576c29facd5d271d9b124ebdafcb
rocioalbarino <100244315+rocioalbarino@users.noreply.github.com>
2022-09-21 17:45:23 -0300

    Hys

--- ProyectoIntegrador_AlbarinoRocio/FrontEnd/src/app/app-routing.module.ts
+++ ProyectoIntegrador_AlbarinoRocio/FrontEnd/src/app/app-routing.module.ts
@@ -5,7 +5,10 @@ import { NeweducacionComponent } from './components/educacion/neweducacion.compo
 import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
 import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
 import { HomeComponent } from './components/home/home.component';
+import { EditSkillComponent } from './components/hys/edit-skill.component';
+import { NewSkillComponent } from './components/hys/new-skill.component.ts ';
 import { LoginComponent } from './components/login/login.component';
+
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -13,7 +16,9 @@ const routes: Routes = [
   { path: 'nuevaexp', component: NewExperienciaComponent },
   { path: 'editexp/:id', component: EditExperienciaComponent},
   { path: 'nuevaedu', component: NeweducacionComponent},
-  { path: 'editedu/:id', component: EditeducacionComponent}
+  { path: 'editedu/:id', component: EditeducacionComponent},
+  { path: 'newskill', component: NewSkillComponent},
+  { path: 'editskill/:id', component: EditSkillComponent}
 ];
 
 @NgModule({
